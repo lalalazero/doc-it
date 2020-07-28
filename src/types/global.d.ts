0494c97d4f529b9fab618a8fd38c1ada7b5cfd1d
lalalazero <zzzero520@hotmail.com>
2020-07-28 17:55:05 +0800

    clean template file

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,17 +1,4 @@
-declare module '*.css' {
-    const content: any;
-    export default content
-}
-
-declare module '*.less' {
-    const content: any;
-    export default content;
-}
 declare module '*.scss' {
-    const content: any;
-    export default content;
-}
-declare module '*.sass' {
     const content: any;
     export default content;
 }
