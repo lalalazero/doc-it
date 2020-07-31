21c51d0847d81067fb3bea8c36c52c075d973054
lalalazero <zzzero520@hotmail.com>
2020-07-31 13:38:51 +0800

    feat: dev and build with webpack

--- src/bin/doc-it.js
+++ src/bin/doc-it.js
@@ -2,18 +2,19 @@
 
 const fs = require('fs')
 const webpack = require('webpack')
+const webpackDevServer = require('webpack-dev-server')
 const program = require('commander')
+
+const defaultDevConfig = require('../../build/webpack.config.dev')
+const defaultProdConfig = require('../../build/webpack.config.prod')
 
 program.command('dev')
     .description('启动 webpack-dev-server 生成文档')
     .option('-c, --config','specify config file location, default to .docitrc.js', '.docitrc.js')
     .option('-d, --dist', 'specify output directory, default to docit', 'docit')
     .action(function(cmd, env) {
-        console.log('cmd')
-        console.log(cmd)
-        console.log('env')
-        console.log(env)
-        console.log('dev dev dev')
+        console.log('dev, dev, dev')
+        doDev()
     })
 
 program.command('build')
@@ -22,9 +23,33 @@ program.command('build')
     .option('-d, --dist', 'specify output directory, default to docit', 'docit')
     .action(function(cmd, env) {
         console.log('build build build')
+        doBuild()
     })
     
 program.parse(process.argv)
+
+
+function doDev() {
+    const compiler = webpack(defaultDevConfig)
+    const devServerOptions = defaultDevConfig.devServer
+    const devServer = new webpackDevServer(compiler, devServerOptions)
+    devServer.listen(3007, 'localhost', () => {
+        console.log('[DOC-IT CLI] start server on http://localhost:3007')
+    })
+}
+
+function doBuild() {
+    webpack(defaultProdConfig, function(err, stats) {
+        if (err) {
+            throw err
+        }
+        if (stats.hasErrors()) {
+            console.log('[DOC-IT CLI]', stats.toString())
+        }
+        process.stdout.write(stats.toString() + '\n\n')
+    })
+    
+}
 
 // let template = `<!DOCTYPE html>
 // <html lang="en">
