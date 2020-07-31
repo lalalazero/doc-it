a44e6ff489f0ba1a7b80f7189a7020b5b71ebebe
lalalazero <zzzero520@hotmail.com>
2020-07-31 15:56:45 +0800

    doc-it dev works anywhere

--- src/bin/doc-it.js
+++ src/bin/doc-it.js
@@ -5,41 +5,42 @@ const path = require('path')
 const webpack = require('webpack')
 const webpackDevServer = require('webpack-dev-server')
 const program = require('commander')
-const webpackMerge = require('webpack-merge')
 
-const defaultDevConfig = getDefaultDevConfig()
+const defaultDevConfig = require('../../build/webpack.config.dev')
 
 program.command('dev')
     .description('启动 webpack-dev-server 生成文档')
-    .option('-c, --config','specify config file location, default to .docitrc.js', '.docitrc.js')
+    .option('-c, --config', 'specify config file location, default to .docitrc.js', '.docitrc.js')
     .option('-d, --dist', 'specify output directory, default to docit', 'docit')
-    .action(function(cmd, env) {
+    .action(function (cmd, env) {
         console.log('dev, dev, dev')
         doDev(cmd, env)
     })
 
 program.command('build')
     .description('启动 webpack 打包生成文档')
-    .option('-c, --config','specify config file location, default to .docitrc.js', '.docitrc.js')
+    .option('-c, --config', 'specify config file location, default to .docitrc.js', '.docitrc.js')
     .option('-d, --dist', 'specify output directory, default to docit', 'docit')
-    .action(function(cmd, env) {
+    .action(function (cmd, env) {
         console.log('build build build')
         doBuild(cmd, env)
     })
-    
+
 program.parse(process.argv)
 
 
 function doDev(cmd, env) {
     let p = path.parse(process.env.PWD)
-    let configFilePath = path.resolve(p.dir, p.base, '.docitrc.js')
-    let configs = require(configFilePath)
-    let outputPath = path.resolve(p.dir, p.base, configs.outputDir)
-    defaultDevConfig.output.path = outputPath
-    defaultDevConfig.devServer.contentBase = outputPath
-    defaultDevConfig.entry = path.resolve(__dirname, '..', '..', 'example/index.tsx')
-    console.log(JSON.stringify(defaultDevConfig,null,2))
-    
+    fs.access(path.join(p.dir, p.base, '.docitrc.js'), function (err) {
+        if (!err) {
+            let configFilePath = path.resolve(p.dir, p.base, '.docitrc.js')
+            let configs = require(configFilePath)
+            let outputPath = path.resolve(p.dir, p.base, configs.outputDir)
+            defaultDevConfig.output.path = outputPath
+            defaultDevConfig.devServer.contentBase = outputPath
+        }
+    })
+
     const compiler = webpack(defaultDevConfig)
     const devServerOptions = defaultDevConfig.devServer
     const devServer = new webpackDevServer(compiler, devServerOptions)
@@ -48,14 +49,8 @@ function doDev(cmd, env) {
     })
 }
 
-function getDefaultDevConfig() {
-    const baseConfig = require('../../build/webpack.config.base')
-    const devConfig = require('../../build/webpack.config.dev')
-    return webpackMerge(baseConfig, devConfig) 
-}
-
 function doBuild(cmd, env) {
-    webpack(defaultProdConfig, function(err, stats) {
+    webpack(defaultProdConfig, function (err, stats) {
         if (err) {
             throw err
         }
@@ -64,5 +59,5 @@ function doBuild(cmd, env) {
         }
         process.stdout.write(stats.toString() + '\n\n')
     })
-    
+
 }
